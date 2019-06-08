e648e9809f5c317312e0cbf1dd63dbc8e3d92269
Youngmin Seo <zzeromin@gmail.com>
2019-06-08 17:04:11 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "dtparam=spi=on" >> /boot/config.txt
+pip install mfrc522
+cp rfid.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl start rfid.service
+systemctl enable rfid.service
+chmod 755 /home/pi/MPD-RFID/src/*.sh
+echo "MPD-RFID Setup Complete. Reboot after 3 Seconds."
+sleep 3
+reboot
