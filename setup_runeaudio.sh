0a3282d7b03dec26a581a46266e9b90bd19b3114
Youngmin Seo <zzeromin@gmail.com>
2019-06-13 17:39:13 +0900

    Create setup_runeaudio.sh

---
+++ setup_runeaudio.sh
@@ -0,0 +1,11 @@
+sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+pip install mfrc522
+cp /root/MPD-RFID/src/rfid.service /lib/systemd/system/
+sed -i 's/home\/pi/root/g' /lib/systemd/system/rfid.service
+systemctl daemon-reload
+systemctl start rfid.service
+systemctl enable rfid.service
+chmod 755 /root/MPD-RFID/src/*.sh
+echo "MPD-RFID Setup Complete. Reboot after 3 Seconds."
+sleep 3
+reboot
