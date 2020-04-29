e71ac0489e75275bd8565dd3748371c000ae87e7
Youngmin Seo <zzeromin@gmail.com>
2020-04-29 13:26:44 +0900

    changed function name

--- dataswap.py
+++ dataswap.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def artistSwap(card):
-    artistMap = {
+# MPD-RFID Data(TAG) online sheet
+# https://bit.ly/3dBzLv0
+
+def cardSwap(card):
+    dataMap = {
         ### 데이터 추가 방법 ###
         ### "카드명":"한글가수명", ###
+        "iu":"아이유",
         "bts":"방탄소년단",
+        "redvelvet":"레드벨벳",
         "bol4":"볼빨간사춘기",
         "psy":"싸이",
         "cool":"쿨",
+        "apink":"에이핑크",
+        "taeyeon":"태연",
+        "twice":"트와이스",
+        "mamamoo":"마마무",
+        "izone":"아이즈원",
+        "sejeong":"세정",
     }
-    return artistMap.get(card, card)
+    return dataMap.get(card, card)
 
 def tagSwap(tag):
     tagMap = {
