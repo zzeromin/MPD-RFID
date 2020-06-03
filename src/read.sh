0323697d770f593a658d9965e564c3728015a554
Youngmin Seo <zzeromin@gmail.com>
2020-06-03 14:36:48 +0900

    Update read.sh

--- src/read.sh
+++ src/read.sh
@@ -1,6 +1,6 @@
 sudo systemctl stop rfid.service
 echo "Take your RFID card to the reader"
-python3 read.py
+python3 /home/pi/MPD-RFID/src/read.py
 sleep 1
 sudo systemctl start rfid.service
 echo "data write successfully!"
