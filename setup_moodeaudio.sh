5d6da70f3f11966e828ead0dee4021c83c901fb6
Youngmin Seo <zzeromin@gmail.com>
2020-06-06 17:57:04 +0900

    Update setup_moodeaudio.sh

--- setup_moodeaudio.sh
+++ setup_moodeaudio.sh
@@ -1,5 +1,6 @@
 echo "dtparam=spi=on" >> /boot/config.txt
 pip3 install mfrc522
+pip3 install pi-rc522
 cp /home/pi/MPD-RFID/src/rfid.service /lib/systemd/system/
 systemctl daemon-reload
 systemctl start rfid.service
