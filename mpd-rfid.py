479730f2a004637380c85e742011226b92cd678e
Youngmin Seo <zzeromin@gmail.com>
2020-06-06 09:15:54 +0900

    Solved high cpu load problem

--- mpd-rfid.py
+++ mpd-rfid.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Title         : mpd-rfid.py
@@ -9,21 +9,29 @@ Thanks to     : Tentacle Team, Raspigamer, MASIL, Doksumaker Cafe
 Special thanks to : rolex member of Tentacle Team, you inspire me :)
 Notice        :
 MPD-RFID run on RuneAudio and MoodeAudio, a custom built GNU Linux operating system for Raspberry Pi.
-installed python package: mfrc522
+installed python package: mfrc522, pi-rc522
 Free and open for all to use. But put credit where credit is due.
 """
 
 import os
+import signal
 import time
+import sys
 import RPi.GPIO as GPIO
 import dataswap
 from mfrc522 import SimpleMFRC522
+from pirc522 import RFID
 
 # Raspberry Pi pin configuration:
-piezo = 23
-GPIO.setmode(GPIO.BCM)
+piezo = 16
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(piezo, GPIO.OUT)
+
+run = True
+rdr = RFID()
+util = rdr.util()
+util.debug = True
 
 # RFID tags and information
 duplication = True # check whether RFID recognizes duplicate cards
@@ -45,47 +53,61 @@ def piezoBeep():
     time.sleep(0.01)
     piezo_pwm.stop()
 
+def end_read(signal,frame):
+    global run
+    print("\nCtrl+C captured, ending read.")
+    run = False
+    rdr.cleanup()
+    sys.exit()
+
+signal.signal(signal.SIGINT, end_read)
+
 # main code
 def main():
     reader = SimpleMFRC522()
     card = ""
     oldCard = ""
 
-    while True:
-        id, card = reader.read()
-        card = card.replace(" ", "")
-        card = card.lower()
-        print("read.card = " + card)
+    while run:
+        rdr.wait_for_tag()
 
-        tag = card[0:3] # parse tag value in rfid card
-        tag = dataswap.tagSwap(tag)
-        print("tag =" + tag)
+        (error, data) = rdr.request()
+        if not error:
+            print("\nDetected: " + format(data, "02x"))
+            id, card = reader.read()
+            card = card.replace(" ", "")
+            card = card.lower()
+            print("read.card = " + card)
 
-        if card in mpc_commands:
-            os.system("mpc " + card)
+            tag = card[0:3] # parse tag value in rfid card
+            tag = dataswap.tagSwap(tag)
+            print("tag =" + tag)
 
-        elif tag in tag_list:
-            card = card[4:] # parse card value in rfid card
-            card = dataswap.cardSwap(card)
-            print("card = "+ card)
+            if card in mpc_commands:
+                os.system("mpc " + card)
 
-            if duplication:
-                piezoBeep()
-                mpdControl(tag, card)
-                oldCard = card
-                
-            else:
-                if oldCard != card:
+            elif tag in tag_list:
+                card = card[4:] # parse card value in rfid card
+                card = dataswap.cardSwap(card)
+                print("card = "+ card)
+
+                if duplication:
                     piezoBeep()
                     mpdControl(tag, card)
                     oldCard = card
-                    print("oldcard = "+ oldCard)
 
-        else:
-            pass
+                else:
+                    if oldCard != card:
+                        piezoBeep()
+                        mpdControl(tag, card)
+                        oldCard = card
+                        print("oldcard = "+ oldCard)
 
-        card = ""
-        time.sleep(1)
+            else:
+                pass
+
+            card = ""
+            time.sleep(1)
 
 if __name__ == "__main__":
     import sys
