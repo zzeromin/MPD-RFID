641d7879caf81973ebee2bf76c96272ac31858a0
Youngmin Seo <zzeromin@gmail.com>
2020-06-11 17:00:57 +0900

    Update setup_runeaudio.sh

--- setup_runeaudio.sh
+++ setup_runeaudio.sh
@@ -1,3 +1,5 @@
+pip install --upgrade pip
+pacman -Syy
 sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
 pip3 install mfrc522
 pip3 install pi-rc522
@@ -7,6 +9,10 @@ systemctl daemon-reload
 systemctl start rfid.service
 systemctl enable rfid.service
 chmod 755 /root/MPD-RFID/src/*.sh
+sed -i 's/sudo //g' ~/MPD-RFID/src/read.sh
+sed -i 's/home\/pi/root/g' ~/MPD-RFID/src/read.sh
+sed -i 's/sudo //g' ~/MPD-RFID/src/write.sh
+sed -i 's/home\/pi/root/g' ~/MPD-RFID/src/write.sh
 echo "MPD-RFID Setup Complete. Reboot after 3 Seconds."
 sleep 3
 reboot
