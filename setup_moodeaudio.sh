c0a589135abc56c447cc7799c8aae4b8f7d8ef29
Youngmin Seo <zzeromin@gmail.com>
2020-06-11 17:02:49 +0900

    Update setup_moodeaudio.sh

--- setup_moodeaudio.sh
+++ setup_moodeaudio.sh
@@ -1,18 +1,12 @@
 pip install --upgrade pip
-pacman -Syy
-sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+echo "dtparam=spi=on" >> /boot/config.txt
 pip3 install mfrc522
 pip3 install pi-rc522
-cp /root/MPD-RFID/src/rfid.service /lib/systemd/system/
-sed -i 's/home\/pi/root/g' /lib/systemd/system/rfid.service
+cp /home/pi/MPD-RFID/src/rfid.service /lib/systemd/system/
 systemctl daemon-reload
 systemctl start rfid.service
 systemctl enable rfid.service
-chmod 755 /root/MPD-RFID/src/*.sh
-sed -i 's/sudo //g' ~/MPD-RFID/src/read.sh
-sed -i 's/home\/pi/root/g' ~/MPD-RFID/src/read.sh
-sed -i 's/sudo //g' ~/MPD-RFID/src/write.sh
-sed -i 's/home\/pi/root/g' ~/MPD-RFID/src/write.sh
+chmod 755 /home/pi/MPD-RFID/src/*.sh
 echo "MPD-RFID Setup Complete. Reboot after 3 Seconds."
 sleep 3
 reboot
